7786a4abf5e745e1f515b413eca7e0742295f389
aarahada14 <aarahada933@gmail.com>
2024-11-19 15:22:45 +0530

    fist day

---
+++ DAY_1/intro2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   
+   int a = 25;
+   int b = 2;
+
+   cout << "addition of a & b :" << a+b <<endl;
+   cout << "substraction of a & b :" << a-b <<endl;
+   cout << "multiplication of a & b :" << a*b <<endl;
+   cout << "divion of a & b :" << a/b <<endl;
+   cout << "module of a & b :" << a%b <<endl;
+
+ }
---
+++ DAY_1/pattern.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   
+   int a = 25;
+   int b = 2;
+
+   cout << "          *   "  <<endl;
+   cout << "        *   * "  <<endl;
+   cout << "      *      * "  <<endl;
+   cout << "     *        * "  <<endl;
+   cout << "    *          * "  <<endl;
+   cout << "   * * * * * *  * "  <<endl;
+   cout << "  *               * "  <<endl;
+   cout << " *                 * "  <<endl;
+   cout << "*                   * "  <<endl;
+  
+
+ }
