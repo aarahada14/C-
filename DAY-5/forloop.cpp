f297bedf68392678b9d6fbef7c34581c1c33e4fc
aarahada14 <aarahada933@gmail.com>
2024-11-22 15:39:41 +0530

    day 5

---
+++ DAY-5/forloop.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int start, end;
+cout << "start: ";
+cin >> start;
+
+cout << "end: ";
+cin >> end;
+
+
+    for(int i = start; i<=end;  i++){
+
+        if(i%2==0){   
+            cout << i << endl;
+       }
+ 
+}
+    return 0;
+}
---
+++ DAY-5/while.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main (){
+
+int i=5;
+int sum =0;
+
+    
+    
+                         
+    while(i <= 10){
+       i++;     
+        sum = sum + i;
+
+          
+    }
+
+
+    cout << sum;
+
+
+
+    return 0;
+}
