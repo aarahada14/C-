870bbdbe9c4bf36fa35e70d8ce89414865c7ac42
aarahada14 <aarahada933@gmail.com>
2024-11-25 15:59:31 +0530

    pattern

---
+++ DAY_6/pattern.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // for(int i=1; i<=5; i++){
+    //     for(int j=1; j<=i; j++){
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+    // for(int i=1; i<=5; i++){
+    //     for(int j=5; j>=i; j--){
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+    // for(int i=1; i<=5; i++){
+    //     for(int j=1; j<=i; j++){
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+    // for(int i=1; i<=5; i++){
+    //     for(int j=1; j<=5; j++){
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+    // for(int i=1; i<=5; i++){
+    //     for(int j=1; j<=5; j++){
+    //         cout << i << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+    // for(int i=1; i<=5; i++){
+    //         for(int j=1; j<=i; j++){
+    //             cout  <<"*" << " ";
+    //         }
+    //         cout << endl;
+    //     }
+
+    for(int i=1; i<=5; i++){
+        for(int j=5; j>=i; j--){
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+
+
+
+
+
+     return 0   ;
+
+
+}  
